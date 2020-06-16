38883531b391ba2bbfefdc9b87ae0a4c0de705fc
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-06-16 12:01:33 +0200

    Add files via upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,3 @@
+
+data.raw["beacon"]["beacon"].graphics_set.animation_list[3].animation           .run_mode = "backward"
+data.raw["beacon"]["beacon"].graphics_set.animation_list[3].animation.hr_version.run_mode = "backward"
